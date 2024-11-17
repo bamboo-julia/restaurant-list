963721ae1b99bfd056ec65007a0e6615be252cc9
bamboo-julia <julialeexxxx@gmail.com>
2024-11-17 16:13:45 +0800

    路由配置 & 載入靜態檔案

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`restaurant: ${id}`)
 })
 
 app.listen(port, () => {
