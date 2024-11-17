c19fa1191835914b3136a71867176893ad18f926
bamboo-julia <julialeexxxx@gmail.com>
2024-11-17 16:42:16 +0800

    載入handlebars

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res) => {
